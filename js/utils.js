19e69c4116e04bb79a8442982f6c8154f28bf376
Parvaneh Yaghoubi <yaghoubip6@gmail.com>
2025-11-07 21:51:42 +0330

    create utils and time ago function

---
+++ js/utils.js
@@ -0,0 +1,8 @@
+export function timeAgo(iso) {
+    if (!iso) return 'unknown';
+    const diff = (Date.now() - new Date(iso)) / 1000;
+    if (diff < 60) return 'just now';
+    if (diff < 3600) return `${Math.floor(diff / 60)} min ago`;
+    if (diff < 86400) return `${Math.floor(diff / 3600)} hr ago`;
+    return `${Math.floor(diff / 86400)} days ago`;
+};
